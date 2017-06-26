998ba576428e69866a4bea05efcd70320ad7d501
Sandeepa1995 <damithalenadora@yahoo.com.au>
2017-06-26 10:44:39 +0530

    Added more functions

---
+++ js/creator.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Damitha on 6/22/2017.
+ */
+function doNew() {
+    if (document.getElementById("rbReg").checked)
+    {
+        location.href='creatorRegular.html';
+    }
+
+}
