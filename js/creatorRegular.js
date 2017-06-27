6c975bec2a160dc988f6ac17bd5aa1f788604cf8
Sandeepa1995 <damithalenadora@yahoo.com.au>
2017-06-27 12:14:04 +0530

    Basic Creation Cycle

---
+++ js/creatorRegular.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Damitha on 6/26/2017.
+ */
+function doTarget() {
+    if (document.getElementById("rbSpc").checked)
+    {
+        showSpeci();
+    }
+    else
+    {
+        hideSpeci();
+    }
+
+}
+
+function showSpeci() {
+    document.getElementById("specificOptions").style.display='block';
+}
+
+function hideSpeci() {
+    document.getElementById("specificOptions").style.display='none';
+}
+
+function finish() {
+    if(confirm('Do you want to finalize the notice?'))
+    {
+        location.href='index.html';
+    }
+}
