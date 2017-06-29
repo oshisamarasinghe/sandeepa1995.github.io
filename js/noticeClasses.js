2b604be486a1cf864f2e98ee3c7e7385cdefa46e
Sandeepa1995 <damithalenadora@yahoo.com.au>
2017-06-29 23:52:56 +0530

    Begin back end coding

---
+++ js/noticeClasses.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Damitha on 6/28/2017.
+ */
+function Notice(iD,title,content,target,duration,type) {
+    this.iD=iD;
+    this.type=type;
+    this.title=title;
+    this.content=content;
+    this.target=target;
+    this.duration=duration;
+
+
+
+}
